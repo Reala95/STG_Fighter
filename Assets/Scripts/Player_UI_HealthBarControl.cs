7602d448985159076b64053f458744af77303ff1
Reala95 <realaziqi@gmail.com>
2020-07-23 19:26:22 -0400

    Health Colar Update

--- Assets/Scripts/Player_UI_HealthBarControl.cs
+++ Assets/Scripts/Player_UI_HealthBarControl.cs
@@ -1,16 +1,22 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
+using UnityEditor.Media;
 using UnityEngine;
 
 public class Player_UI_HealthBarControl : MonoBehaviour
 {
-    GameObject healthBar;
+    public AnimationCurve redCurve;
+    public AnimationCurve greenCurve;
+    public AnimationCurve blueCurve;
     Common_HP playerHP;
+    SpriteRenderer healthBarRenderer;
 
     // Start is called before the first frame update
     void Start()
     {
         playerHP = GameObject.FindGameObjectWithTag("Player").GetComponent<Common_HP>();
+        healthBarRenderer = transform.GetComponentInChildren<SpriteRenderer>();
     }
 
     // Update is called once per frame
@@ -21,6 +27,11 @@ public class Player_UI_HealthBarControl : MonoBehaviour
 
     private void FixedUpdate()
     {
-        transform.localScale = new Vector3((float) playerHP.getCurrentHP() / playerHP.getMaxHP(), 1, 1);
+        float ratio = (float) playerHP.getCurrentHP() / playerHP.getMaxHP();
+        transform.localScale = new Vector3(ratio, 1, 1);
+        float r = redCurve.Evaluate(Math.Min(1, (1 - ratio) * 2));
+        float g = greenCurve.Evaluate(Math.Min(1, ratio * 2));
+        float b = 0;
+        healthBarRenderer.color = new Color(r, g, b);
     }
 }
